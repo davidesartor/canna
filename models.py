48f0ab8e724e1ce0d34813bcba136ce7478c7d47
davidesartor <davide.sartor.4@studenti.unipd.it>
2024-12-08 22:06:55 +0100

    rewrote model.fit for eqx

--- models.py
+++ models.py
@@ -4,7 +4,8 @@ from tqdm import tqdm
 
 
 class MLPCNF(eqx.Module):
-    layers: list[eqx.nn.Linear]
+    linear_layers: list[eqx.nn.Linear]
+    norm_layers: list[eqx.nn.RMSNorm | eqx.nn.Identity]
 
     def __init__(
         self,
@@ -14,35 +15,30 @@ class MLPCNF(eqx.Module):
         depth: int = 4,
         norm: bool = True,
         *,
-        rng: Key
+        rng: Key,
     ):
-        dims=[x_dim+y_dim+1]+[hidden for _ in range(depth-1)]+[x_dim]
-        rngs=jr.split(rng,depth)
-        self.layers=[
-            eqx.nn.Linear(in_dim,out_dim,key=k)
-            for in_dim, out_dim, k in zip(dims[:-1],dims[1:],rngs)
+        dims = [x_dim + y_dim + 1] + [hidden for _ in range(depth - 1)] + [x_dim]
+        self.norm_layers = [
+            eqx.nn.RMSNorm(hidden) if norm else eqx.nn.Identity()
+            for _ in range(depth - 1)
+        ]
+        self.linear_layers = [
+            eqx.nn.Linear(in_dim, out_dim, key=k)
+            for in_dim, out_dim, k in zip(dims[:-1], dims[1:], jr.split(rng, depth))
         ]
 
-        
-    
     def __call__(
         self, t: Scalar, xt: Float[Array, "xt_flat"], y: Float[Array, "y_flat"]
     ) -> Float[Array, "xt_flat"]:
         h = jnp.concatenate([t[..., None], xt, y], axis=-1)
-        for layer in self.layers[:-1]:
-            h = layer(h)
+        for linear, norm in zip(self.linear_layers[:-1], self.norm_layers):
+            h = linear(h)
             h = jax.nn.silu(h)
-            #if self.norm:
-            #    h = nn.RMSNorm()(h)
-        h = self.layers[-1](h)
+            h = norm(h)
+        h = self.linear_layers[-1](h)
         return h
 
-    def loss(self, batch):
-        t, xt, dx, y = batch
-        pred = jax.vmap(self)(t, xt, y)
-        return optax.l2_loss(pred, dx).mean()
-
-    def push(self, x0: Float[Array, "n x"], y: Float[Array, "y"], n_steps=4):
+    def push(self, x0: Float[Array, "x"], y: Float[Array, "y"], n_steps=4):
         def runje_kutta_step(x, t):
             k1 = self(t, x, y)
             k2 = self(t + dt / 2, x + k1 * dt / 2, y)
@@ -56,7 +52,6 @@ class MLPCNF(eqx.Module):
         x1, xt = jax.lax.scan(runje_kutta_step, x0, ts)
         return x1, xt
 
-    #@nn.nowrap
     def fit(
         self,
         dataset: PosteriorFlowDataset,
@@ -68,39 +63,39 @@ class MLPCNF(eqx.Module):
         seed=0,
     ):
         rng = jr.key(seed)
-        rng, rng_init = jr.split(rng)
-        params = self.init(rng_init, dataset.train_sample(jr.key(0)), method="loss")
         optimizer = optax.adamw(learning_rate, weight_decay=weight_decay)
-        opt_state = optimizer.init(params)
+        opt_state = optimizer.init(eqx.filter(self, eqx.is_array))
 
         @jax.jit
         def generate_batches(rng):
             get_batch = lambda rng: dataset.train_batch(rng, batch_size)
             return jax.vmap(get_batch)(jr.split(rng, steps_per_epoch))
 
-        @jax.jit
-        def train_epoch(params, opt_state, batches):
-            def optimization_step(carry, batch):
-                @jax.value_and_grad
-                def loss_fn(params, batch):
-                    return self.apply(params, batch, method="loss")
+        @jax.value_and_grad
+        def loss_fn(flow, batch):
+            t, xt, dx, y = batch
+            pred = jax.vmap(flow)(t, xt, y)
+            return optax.l2_loss(pred, dx).mean()
 
-                params, opt_state = carry
-                loss, grads = loss_fn(params, batch)
-                updates, opt_state = optimizer.update(grads, opt_state, params)
-                params = optax.apply_updates(params, updates)
-                carry = (params, opt_state)
+        @eqx.filter_jit
+        def train_epoch(flow, opt_state, batches):
+            def optimization_step(carry, batch):
+                flow, opt_state = carry
+                loss, grads = loss_fn(flow, batch)
+                updates, opt_state = optimizer.update(grads, opt_state, flow)
+                flow = eqx.apply_updates(flow, updates)
+                carry = (flow, opt_state)
                 return carry, loss
 
-            (params, opt_state), losses = jax.lax.scan(
-                optimization_step, (params, opt_state), batches
+            (flow, opt_state), losses = jax.lax.scan(
+                optimization_step, (flow, opt_state), batches
             )
-            return params, opt_state, losses
+            return flow, opt_state, losses
 
         loss_log = []
         for rng in (pbar := tqdm(jr.split(rng, epochs))):
             batches = generate_batches(rng)
-            params, opt_state, losses = train_epoch(params, opt_state, batches)
+            self, opt_state, losses = train_epoch(self, opt_state, batches)
             pbar.set_postfix(loss=losses.mean())
             loss_log.append(losses)
-        return params, jnp.array(loss_log)
+        return self, jnp.array(loss_log)
